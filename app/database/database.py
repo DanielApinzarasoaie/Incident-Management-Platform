4e7233a0d97db5e8e83be27656a95ddf16067fff
DanielApinzarasoaie <danut_man98@yahoo.com>
2026-06-26 20:27:54 +0300

    Add CD deployment

--- app/database/database.py
+++ app/database/database.py
@@ -6,11 +6,30 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL : 
+
+    DB_USER = os.getenv("POSTGRES_USER")
+    DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+
+    DATABASE_URL={
+        f"postgresql://"
+        f"{DB_USER}:"
+        f"{DB_PASSWORD}"
+        f"@{DB_HOST}:"
+        f"{DB_PORT}/"
+        f"{DB_NAME}"
+    }
 
 if not DATABASE_URL:
     raise ValueError(
-        "DATABASE_URL environment variable not found"
+        "Database configuration missing"
     )
 
 engine = create_engine(DATABASE_URL)
